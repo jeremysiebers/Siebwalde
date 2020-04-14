1daa0d37eea66f5ee3c27849da2ee057b99e2d02
Jeremy Siebers <jeremysiebers@gmail.com>
2020-04-15 00:26:53 +0200

    Started from scratch following AngelSix WPF tutorials and using parts of his open-source example: https://github.com/angelsix/fasetto-word Converting main window to PPTX example, try later to incorporate the fiddle yard application part into code and to startup the winforms via code.

---
+++ SiebwaldeApp/SiebwaldeApp/ValueConverters/ApplicationPage.cs
@@ -0,0 +1,17 @@
+﻿namespace SiebwaldeApp
+{
+    /// <summary>
+    /// A page of the application
+    /// </summary>
+    public enum ApplicationPage
+    {
+        /// <summary>
+        /// The initial login page
+        /// </summary>
+        TrackControlView = 0,
+
+        TrackAmplifierItemView = 1,
+
+        TrackAmplifierManualControlView = 2
+    }
+}
