0be1506e3527b34a57609fbf174402a91d2ca8e6
Jeremy Siebers <jeremysiebers@gmail.com>
2022-12-16 00:32:55 +0100

    jumped to new PIC: 16F-LF-15324. Updated schematic.

--- Faller_Car_ucontroller.X/Main.h
+++ Faller_Car_ucontroller.X/Main.h
@@ -4,9 +4,6 @@
 #define On							1	
 #define Off							0
 
-#define True                        1
-#define False                       0
-
 #define Input                       1
 #define Output                      0
 
