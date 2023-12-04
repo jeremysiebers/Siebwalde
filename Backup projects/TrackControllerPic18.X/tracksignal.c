b7fe2ea3ec530989ffe4f475edca295ab6dbd67b
Jeremy Siebers <jeremysiebers@gmail.com>
2023-12-04 17:34:37 +0100

    test

--- Backup projects/TrackControllerPic18.X/tracksignal.c
+++ Backup projects/TrackControllerPic18.X/tracksignal.c
@@ -8,6 +8,7 @@
  */
 void SETxSIGNAL(STATION *self, uint8_t path, enum STATES signal)
 {
+#pragma optimize( "", off )
     uint8_t pin1 = self->setPath->pin1_mask;
     uint8_t pin2 = self->setPath->pin2_mask;
     uint8_t pin3 = self->setPath->pin3_mask;
@@ -18,34 +19,35 @@ void SETxSIGNAL(STATION *self, uint8_t path, enum STATES signal)
     switch(signal){
         case SIG_RED:
             if(path == 1 || path == 10){
-                *self->setSignal->port1_ptr &= ~pin1; // Green
-                *self->setSignal->port2_ptr |=  pin2; // Red
+                *self->setSignal->port1_ptr &= ~self->setPath->pin1_mask; // Green
+                *self->setSignal->port2_ptr |=  self->setPath->pin2_mask; // Red
             }
             else if(path == 2 || path == 11){
-                *self->setSignal->port3_ptr &= ~pin3; // Green
-                *self->setSignal->port4_ptr |=  pin4; // Red
+                *self->setSignal->port3_ptr &= ~self->setPath->pin3_mask; // Green
+                *self->setSignal->port4_ptr |=  self->setPath->pin4_mask; // Red
             }
             else if(path == 3 || path == 12){
-                *self->setSignal->port5_ptr &= ~pin5; // Green
-                *self->setSignal->port6_ptr |=  pin6; // Red
+                *self->setSignal->port5_ptr &= ~self->setPath->pin5_mask; // Green
+                *self->setSignal->port6_ptr |=  self->setPath->pin6_mask; // Red
             }
             break;
             
         case SIG_GREEN:
             if(path == 1 || path == 10){
-                *self->setSignal->port1_ptr |=  pin1; // Green
-                *self->setSignal->port2_ptr &= ~pin2; // Red
+                *self->setSignal->port1_ptr |=  self->setPath->pin1_mask; // Green
+                *self->setSignal->port2_ptr &= ~self->setPath->pin2_mask; // Red
             }
             else if(path == 2 || path == 11){
-                *self->setSignal->port3_ptr |=  pin3; // Green
-                *self->setSignal->port4_ptr &= ~pin4; // Red
+                *self->setSignal->port3_ptr |=  self->setPath->pin3_mask; // Green
+                *self->setSignal->port4_ptr &= ~self->setPath->pin4_mask; // Red
             }
             else if(path == 3 || path == 12){
-                *self->setSignal->port5_ptr |=  pin5; // Green
-                *self->setSignal->port6_ptr &= ~pin6; // Red
+                *self->setSignal->port5_ptr |=  self->setPath->pin5_mask; // Green
+                *self->setSignal->port6_ptr &= ~self->setPath->pin6_mask; // Red
             }
             break;
             
         default: break;
     }
+#pragma optimize( "", on )
 }
