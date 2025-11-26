ac58f2a55f33d49c0d002398f5aa2e59cb2663b9
Jeremy Siebers <jeremysiebers@gmail.com>
2025-11-26 22:58:59 +0100

    Some lost changes

--- SiebwaldeApp/SiebwaldeApp.Core/Model/TrackApplication/Controller/TrackControlMain.cs
+++ SiebwaldeApp/SiebwaldeApp.Core/Model/TrackApplication/Controller/TrackControlMain.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel;
 using System.Timers;
 
-namespace SiebwaldeApp
+namespace SiebwaldeApp.Core
 {
     /// <summary>
     /// This is the main Trackcontroller Application class
